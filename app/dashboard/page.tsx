bb26d154e13c43d5bd8de7dd56c5e07174569ede
RoR9 <valeriu.verebceanu@gmail.com>
2024-10-05 21:49:41 +0300

    converting dashboard to server component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,25 @@
-"use client";
 import { CreateCarDialog } from "@/components/custom/CreateCarDialog";
 import { carColumns } from "@/components/table/columns";
 
 import { DataTable } from "@/components/ui/data-table";
 
 import React from "react";
+
 import CheckExpiryButton from "@/components/custom/ExpiryButton";
-import {
-  QueryClient,
-  useQuery,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import DashboardPage from "@/modules/dashboard";
+import { fetchCars } from "@/app/dashboard/actions";
 
-const queryClient = new QueryClient();
+const Page = async () => {
+  const cars = await fetchCars();
 
-const Page = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <DashboardPage />
-    </QueryClientProvider>
+    <div className="flex p-20 h-full">
+      <div className="max-w-screen-xl mx-auto w-full">
+        <h1 className="text-5xl mb-10">Dashboard</h1>
+        <CreateCarDialog />
+        <DataTable columns={carColumns} data={cars} />
+        <CheckExpiryButton />
+      </div>
+    </div>
   );
 };
 
