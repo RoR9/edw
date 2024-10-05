d02d7186006fb27da94dfbd03a6c0aa5a15aa642
RoR9 <valeriu.verebceanu@gmail.com>
2024-10-05 21:27:28 +0300

    force dynamic into getCars route

--- app/api/getCars/route.ts
+++ app/api/getCars/route.ts
@@ -2,6 +2,8 @@
 import { fetchCars } from "@/app/dashboard/actions";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const cars = await fetchCars();
